b4f55ae56f7dfa8e4c82c9047fcb0263cdebe113
nuttaylove1234 <59478311+nuttaylove1234@users.noreply.github.com>
2020-01-03 21:13:36 +0700

    Create update_apk.php

---
+++ update_apk.php
@@ -0,0 +1,7 @@
+{
+  "versionCode":2,
+  "versionName":"2.0.0",
+  "content":"1.เทสอัพเดท1
+2.เทสอัพเดท2",
+  "minSupport":1, "url":"https://nanugakill.000webhostapp.com/NaNu-VPN%20Version%201.2.7.apk"
+}
